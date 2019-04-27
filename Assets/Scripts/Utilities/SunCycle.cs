f6ff649791b2bc313f2d9ecb469449cab254feff
Tom Hewitt <tommhewitt@gmail.com>
2019-04-27 16:38:36 +0100

    Added a SunCycle for a bit more environmental variety

---
+++ Assets/Scripts/Utilities/SunCycle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utilities
+{
+	public class SunCycle : MonoBehaviour
+	{
+		public Vector3 daySpeed;
+		public Vector3 nightSpeed;
+
+		private Vector3 speed;
+
+		private float angle;
+
+		void Update()
+		{
+			transform.Rotate(speed * Time.deltaTime, Space.World);
+
+			angle = transform.localEulerAngles.z;
+
+			if (angle < 270 && angle > 90)
+				speed = nightSpeed;
+			else
+				speed = daySpeed;
+		}
+	}
+}
