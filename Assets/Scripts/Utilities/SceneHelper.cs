5bb4ff396f3801c9fb7fed4d4517ab7f75478a62
Tom Hewitt <tommhewitt@gmail.com>
2019-04-28 18:12:22 +0100

    Redesigned the Highscore scene to make it portrait and neon-like
    - Used Highscores Profile on the camera to give bloom.
    - Removed HighscoreDisplayHelper and rewrote most of the HighscoreManager to contain it all in a single script.
    - Added SceneHelper to easier scene management.
    - Moved the Dreamlo information to GameSettings.
    - Added HighscoreEntry to keep track of the entries on the leaderboard screen.

---
+++ Assets/Scripts/Utilities/SceneHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// A persistent GameObject for helping with scene management.
+/// </summary>
+namespace Utilities
+{
+	public class SceneHelper : MonoBehaviour
+	{
+		public void LoadScene(string s)
+		{
+			SceneManager.LoadScene(s);
+		}
+	}
+}
