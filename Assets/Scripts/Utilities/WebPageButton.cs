ec3067accf512ab918b6c5813fe715f0f277d45a
Tom Hewitt <tommhewitt@gmail.com>
2019-05-17 18:14:35 +0100

    Added Privacy Policy to the main menu
    - Also added a button to open a webpage linking to the policy

---
+++ Assets/Scripts/Utilities/WebPageButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebPageButton : MonoBehaviour
+{
+    public void OpenInBrowser(string url)
+	{
+		Application.OpenURL(url);
+	}
+}
