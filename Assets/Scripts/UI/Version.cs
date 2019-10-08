99b2c00356ef3e7e28d7a9d4f2f42eeab2a1a8ec
Tom Hewitt <tommhewitt@gmail.com>
2019-10-08 19:07:57 +0100

    Add version script and text to main menu

---
+++ Assets/Scripts/UI/Version.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+	public class Version : MonoBehaviour
+	{
+		private void Start()
+		{
+			GetComponent<Text>().text = "Version: " + Application.version;
+		}
+	}
+}
