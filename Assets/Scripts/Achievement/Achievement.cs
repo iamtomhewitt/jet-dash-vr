195a9244979c5f2b837a6c74c6e237e636bcb36a
Tom Hewitt <tommhewitt@gmail.com>
2019-10-09 21:23:09 +0100

    Add ability to progress and unlock achievements

--- Assets/Scripts/Achievement/Achievement.cs
+++ Assets/Scripts/Achievement/Achievement.cs
@@ -1,36 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [System.Serializable]
 public class Achievement
 {
-	public int id;
 	public string achievementName;
+	public int id;
 	public int awardValue;
 	public float progressPercentage;
+	public bool unlocked;
 
 	public Achievement(int id, string name, int value)
 	{
 		this.id = id;
 		this.achievementName = name;
 		this.awardValue = value;
+		this.unlocked = false;
+	}
+
+	/// <summary>
+	/// Progresses the achievement to a certain percentage.
+	/// </summary>
+	public void Progress(float percentage)
+	{
+		if (this.unlocked)
+		{
+			return;
+		}
+
+		this.progressPercentage = percentage;
+
+		if (this.progressPercentage >= 100f)
+		{
+			Debug.Log("'" + this.achievementName + "' unlocked!");
+			this.unlocked = true;
+		}
+	}
+
+	/// <summary>
+	/// Instantly unlocks an achievement.
+	/// </summary>
+	public void Unlock()
+	{
+		Progress(100f);
+	}
+
+	public int GetId()
+	{
+		return this.id;
 	}
 }
-
-/// <summary>
-/// Achievement ideas:
-/// 
-/// - Die
-/// - Get a new highscore
-/// - Upload a highscore
-/// - Play in VR mode
-/// - Get a distance further than X, then Y, then Z
-/// - Get bonus points
-/// - Get double points
-/// - Get invincibility
-/// - Fly through an obstacle whilst invincible
-/// - Achieve max speed
-/// 
-/// </summary>
-
--- Assets/Scripts/Achievement/AchievementManager.cs
+++ Assets/Scripts/Achievement/AchievementManager.cs
@@ -1,8 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
+using System.Linq;
 
 public class AchievementManager : MonoBehaviour
 {
@@ -16,6 +15,9 @@ public class AchievementManager : MonoBehaviour
 		achievements = LoadAchievementsFromFile();
 	}
 
+	/// <summary>
+	/// Returns the achivements saved in a JSON file.
+	/// </summary>
 	public Achievement[] LoadAchievementsFromFile()
 	{
 		// Generate a new set of achievements if the file does not exist
@@ -30,6 +32,9 @@ public class AchievementManager : MonoBehaviour
 		return AchievementJsonHelper.FromJson(fileContent);
 	}
 
+	/// <summary>
+	/// Saves the achivements as JSON to file.
+	/// </summary>
 	public void SaveAchievementsToFile()
 	{
 		string json = AchievementJsonHelper.ToJson(achievements);
@@ -40,24 +45,47 @@ public class AchievementManager : MonoBehaviour
 	}
 
 	/// <summary>
+	/// Instantly unlocks an achievement.
+	/// </summary>
+	public void UnlockAchievement(int id)
+	{
+		GetAchievement(id).Unlock();
+		SaveAchievementsToFile();
+	}
+
+	/// <summary>
+	/// Progresses an achievement.
+	/// </summary>
+	public void ProgressAchievement(int id, float percentage)
+	{
+		GetAchievement(id).Progress(percentage);
+		SaveAchievementsToFile();
+	}
+
+	public Achievement GetAchievement(int id)
+	{
+		return achievements.Where(x => Equals(x.GetId(), id)).ElementAt(0);
+	}
+
+	/// <summary>
 	/// Used when the achivement file cannot be found / does not exist.
-	/// Generates an array of achievements
+	/// Generates an array of achievements.
 	/// </summary>
 	private Achievement[] GenerateSetOfAchievements()
 	{
-		List<Achievement> listOfAchivements = new List<Achievement>();
-		listOfAchivements.Add(new Achievement(1, "Die", 5));
-		listOfAchivements.Add(new Achievement(2, "Get A New Highscore", 30));
-		listOfAchivements.Add(new Achievement(3, "Upload A Highscore", 15));
-		listOfAchivements.Add(new Achievement(4, "Play In VR Mode", 5));
-		listOfAchivements.Add(new Achievement(5, "Get A Distance Further Than 1000", 5));
-		listOfAchivements.Add(new Achievement(6, "Get A Distance Further Than 10000", 30));
-		listOfAchivements.Add(new Achievement(7, "Get A Distance Further Than 100000", 100));
-		listOfAchivements.Add(new Achievement(8, "Fly Through Bonus Points", 5));
-		listOfAchivements.Add(new Achievement(9, "Fly Through Double Points", 5));
-		listOfAchivements.Add(new Achievement(10, "Become Invincible", 5));
-		listOfAchivements.Add(new Achievement(11, "Fly Through An Obstacle Whilst Invincible", 15));
-		listOfAchivements.Add(new Achievement(12, "Achieve Max Speed", 25));
-		return listOfAchivements.ToArray();
+		List<Achievement> list = new List<Achievement>();
+		list.Add(new Achievement(1, "Die", 5));
+		list.Add(new Achievement(2, "Get A New Highscore", 30));
+		list.Add(new Achievement(3, "Upload A Highscore", 15));
+		list.Add(new Achievement(4, "Play In VR Mode", 5));
+		list.Add(new Achievement(5, "Get A Distance Further Than 1000", 5));
+		list.Add(new Achievement(6, "Get A Distance Further Than 10000", 30));
+		list.Add(new Achievement(7, "Get A Distance Further Than 100000", 100));
+		list.Add(new Achievement(8, "Fly Through Bonus Points", 5));
+		list.Add(new Achievement(9, "Fly Through Double Points", 5));
+		list.Add(new Achievement(10, "Become Invincible", 5));
+		list.Add(new Achievement(11, "Fly Through An Obstacle Whilst Invincible", 15));
+		list.Add(new Achievement(12, "Achieve Max Speed", 25));
+		return list.ToArray();
 	}
 }
