048d738903590e25e69889c5df432cfe85ef8812
Tom Hewitt <tommhewitt@gmail.com>
2019-10-14 21:57:30 +0100

    Reorganise achievement order
    Issue #18

--- Assets/Scripts/Achievement/AchievementDatabase.cs
+++ Assets/Scripts/Achievement/AchievementDatabase.cs
@@ -52,9 +52,11 @@ namespace Achievements
 			return new List<Achievement>
 			{
 				new Achievement(AchievementIds.DIE, "Die", "Crash Into An Obstacle", 5),
+				new Achievement(AchievementIds.PLAY_IN_VR, "Seeing Double", "Play In VR Mode", 5),
+				new Achievement(AchievementIds.GET_MAX_SPEED, "Turbo", "Achieve Max Speed", 25),
+				new Achievement(AchievementIds.WATCH_ADS, "SQUARE EYES", "Watch three adverts", 300),
 				new Achievement(AchievementIds.NEW_HIGHSCORE, "Newbie", "Get A New Highscore", 30),
 				new Achievement(AchievementIds.UPLOAD_HIGHSCORE, "Welcome To The Club", "Upload A Highscore", 15),
-				new Achievement(AchievementIds.PLAY_IN_VR, "Seeing Double", "Play In VR Mode", 5),
 				new Achievement(AchievementIds.DISTANCE_FURTHER_THAN_1000, "Going Places", "Get a distance further than 1000", 5),
 				new Achievement(AchievementIds.DISTANCE_FURTHER_THAN_10000, "To The Moon!", "Get a distance further than 10,000", 30),
 				new Achievement(AchievementIds.DISTANCE_FURTHER_THAN_50000, "Sky Is The Limit", "Get a distance further than 50,000", 200),
@@ -62,11 +64,9 @@ namespace Achievements
 				new Achievement(AchievementIds.FLY_THROUGH_DOUBLE_POINTS, "That's A Double", "Fly Through Double Points", 5),
 				new Achievement(AchievementIds.BECOME_INVINCIBLE, "Unlimited Power!", "Become Invincible", 5),
 				new Achievement(AchievementIds.FLY_THROUGH_OBSTACLE_WHEN_INVINCIBLE, "Indestructable", "Fly Through An Obstacle Whilst Invincible", 15),
-				new Achievement(AchievementIds.GET_MAX_SPEED, "Turbo", "Achieve Max Speed", 25),
 				new Achievement(AchievementIds.POINTS_OVER_HALF_MILLION, "What?", "Earn 500,000 points", 100),
 				new Achievement(AchievementIds.POINTS_OVER_MILLION, "IMPOSSIBLE!", "Earn 1,000,000 points", 500),
-				new Achievement(AchievementIds.POINTS_OVER_FIVE_MILLION, "UNFRIGGENBELIEVEABLE", "Earn 5,000,000 points", 1000),
-				new Achievement(AchievementIds.WATCH_ADS, "SQUARE EYES", "Watch three adverts", 300)
+				new Achievement(AchievementIds.POINTS_OVER_FIVE_MILLION, "UNFRIGGENBELIEVEABLE", "Earn 5,000,000 points", 1000)
 			}.ToArray();
 		}
 	}
