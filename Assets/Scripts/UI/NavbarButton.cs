15e81e6b1e055c52189c6769d73fb40901506540
Tom Hewitt <tommhewitt@gmail.com>
2019-10-21 18:28:44 +0100

    Add a Navbar and a button for controlling scenes from it

---
+++ Assets/Scripts/UI/NavbarButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using LevelManagers;
+
+namespace UI
+{
+	public class NavbarButton : MonoBehaviour
+	{
+		/// <summary>
+		/// Called from a Unity button. Used for the navbar at the bottom of a screen, saves
+		/// us having to hook up a level manager on each scene. This way the prefab will always be
+		/// able to load levels without having to hook anything up in the inspector.
+		/// </summary>
+		public void SwitchLevel(string scene)
+		{
+			LevelManager manager = FindObjectOfType<LevelManager>();
+			manager.OnButtonPressed();
+			manager.LoadLevel(scene);
+		}
+	}
+}
