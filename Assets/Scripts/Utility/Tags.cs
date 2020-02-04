91792eb8e28348abc43660b50413d691b22301f3
Tom Hewitt <tommhewitt@gmail.com>
2020-02-04 21:17:06 +0000

    💻 Add ship name tags

--- Assets/Scripts/Utility/Tags.cs
+++ Assets/Scripts/Utility/Tags.cs
@@ -5,5 +5,10 @@
 		public const string OBSTACLE = "Obstacle";
 		public const string POWERUP = "Powerup";
 		public const string PLAYER = "Player";
+		public const string CELLEX = "Cellex";
+		public const string SOLSTICE = "Solstice";
+		public const string DEVILDOG = "DevilDog";
+		public const string EQUINOX = "Equinox";
+		public const string RESCATE = "Rescate";
 	}
 }
