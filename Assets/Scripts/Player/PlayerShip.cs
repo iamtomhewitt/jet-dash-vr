7b6d5e0c183d082fab019265ed9c4bef536025fa
Tom Hewitt <tommhewitt@gmail.com>
2020-02-04 20:40:55 +0000

    💻 Add a scriptable object for the player ships, which will be displayed in the shop

---
+++ Assets/Scripts/Player/PlayerShip.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+	/// <summary>
+	/// Details about the players ship, which are also displayed in the shop.
+	/// </summary>
+	[CreateAssetMenu()]
+	public class PlayerShip : ScriptableObject
+	{
+		[SerializeField] private string shipName;
+		[SerializeField] private SpecialAbility specialAbility;
+		[SerializeField] private float speed;
+		[SerializeField] private float turnSpeed;
+		[SerializeField] private float cost;
+	}
+
+	/// <summary>
+	/// The names of the special abilities that each of the ships have.
+	/// </summary>
+	public enum SpecialAbility { Slugger, Speedster, Frogger, Hyperdrive, Diamond }
+}
