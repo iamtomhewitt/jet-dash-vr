26dae89377c42f9ec03778b0951d6656157e673e
Tom Hewitt <tommhewitt@gmail.com>
2020-02-05 22:32:46 +0000

    💻 Add a shop level manager to interact with the shop instance via UI elements

---
+++ Assets/Scripts/Level Manager/ShopLevelManager.cs
@@ -0,0 +1,18 @@
+﻿using Manager;
+
+namespace LevelManagers
+{
+	/// <summary>
+	/// Handles everything on the Shop menu.
+	/// </summary>
+	public class ShopLevelManager : LevelManager
+	{
+		/// <summary>
+		/// Called from a Unity button.
+		/// </summary>
+		public void BuyShip(string name)
+		{
+			ShopManager.instance.PurchaseShip(name);
+		}
+	}
+}
