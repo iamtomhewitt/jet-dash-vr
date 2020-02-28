ac0eb9178a8c5082d2a4da6e17db007f2bee0c5d
Tom Hewitt <tommhewitt@gmail.com>
2020-02-28 19:00:58 +0000

    💻 Show the Rescate ship first no matter the list order

--- Assets/Scripts/Level Manager/ShopLevelManager.cs
+++ Assets/Scripts/Level Manager/ShopLevelManager.cs
@@ -6,78 +6,89 @@ using Data;
 
 namespace LevelManagers
 {
-	/// <summary>
-	/// Handles everything on the Shop menu.
-	/// </summary>
-	public class ShopLevelManager : LevelManager
-	{
-		[SerializeField] private Text shipName;
-		[SerializeField] private Text description;
-		[SerializeField] private Text speed;
-		[SerializeField] private Text acceleration;
-		[SerializeField] private Text turnSpeed;
-		[SerializeField] private Text cost;
-		[SerializeField] private Text specialAbility;
-		[SerializeField] private Text specialAbilityDescription;
-		[SerializeField] private Image shipIcon;
-		[SerializeField] private Text cash;
+    /// <summary>
+    /// Handles everything on the Shop menu.
+    /// </summary>
+    public class ShopLevelManager : LevelManager
+    {
+        [SerializeField] private Text shipName;
+        [SerializeField] private Text description;
+        [SerializeField] private Text speed;
+        [SerializeField] private Text acceleration;
+        [SerializeField] private Text turnSpeed;
+        [SerializeField] private Text cost;
+        [SerializeField] private Text specialAbility;
+        [SerializeField] private Text specialAbilityDescription;
+        [SerializeField] private Image shipIcon;
+        [SerializeField] private Text cash;
 
-		private List<ShipData> ships;
-		private int currentShipIndex = 0;
+        private List<ShipData> ships;
+        private int currentShipIndex = 0;
 
-		private void Start()
-		{
-			ships = ShopManager.instance.GetShips();
-			cash.text = "CASH: " + ShopManager.instance.GetCash() + "P";
-			SetShipDetails();
-		}
+        private void Start()
+        {
+            ships = ShopManager.instance.GetShips();
+            cash.text = "CASH: " + ShopManager.instance.GetCash() + "P";
 
-		/// <summary>
-		/// Called from a Unity button.
-		/// </summary>
-		public void BuyShip()
-		{
-			ShipData ship = ships[currentShipIndex];
-			ShopManager.instance.PurchaseShip(ship.GetShipName());
-		}
+			// Set the first ship to be shown the basic ship.
+            for (int i = 0; i < ships.Count; i++)
+            {
+                if (ships[i].GetShipName().Equals("Rescate"))
+                {
+                    currentShipIndex = i;
+                    break;
+                }
+            }
 
-		/// <summary>
-		/// Called from a Unity button.
-		/// </summary>
-		public void ShowNextShip()
-		{
-			currentShipIndex = ++currentShipIndex > ships.Count - 1 ? 0 : currentShipIndex;
-			SetShipDetails();
-		}
+            SetShipDetails();
+        }
 
-		/// <summary>
-		/// Called from a Unity button.
-		/// </summary>
-		public void ShowPreviousShip()
-		{
-			currentShipIndex = --currentShipIndex < 0 ? ships.Count - 1 : currentShipIndex;
-			SetShipDetails();
-		}
+        /// <summary>
+        /// Called from a Unity button.
+        /// </summary>
+        public void BuyShip()
+        {
+            ShipData ship = ships[currentShipIndex];
+            ShopManager.instance.PurchaseShip(ship.GetShipName());
+        }
 
-		private void SetShipDetails()
-		{
-			ShipData shipData 				= ships[currentShipIndex];
-			bool isUnlocked					= ShopManager.instance.IsShipUnlocked(shipData);
+        /// <summary>
+        /// Called from a Unity button.
+        /// </summary>
+        public void ShowNextShip()
+        {
+            currentShipIndex = ++currentShipIndex > ships.Count - 1 ? 0 : currentShipIndex;
+            SetShipDetails();
+        }
 
-			shipName.text 					= shipData.GetShipName();
-			description.text				= shipData.GetDescription();
-			speed.text						= "Speed: " + shipData.GetSpeed().ToString();
-			acceleration.text				= "Acceleration: " + shipData.GetSpeedIncreaseRate().ToString();
-			turnSpeed.text					= "Turn Speed: " + shipData.GetTurningSpeed().ToString();
-			cost.text						= "COST: " + shipData.GetCost().ToString() + "P";
-			specialAbility.text 			= shipData.GetSpecialAbility().ToString();
-			specialAbilityDescription.text 	= shipData.GetSpecialAbilityDescription();
-			shipIcon.sprite					= shipData.GetSprite();
+        /// <summary>
+        /// Called from a Unity button.
+        /// </summary>
+        public void ShowPreviousShip()
+        {
+            currentShipIndex = --currentShipIndex < 0 ? ships.Count - 1 : currentShipIndex;
+            SetShipDetails();
+        }
 
-			if (!isUnlocked)
-			{
-				shipIcon.color = Color.black;
-			}
-		}
-	}
+        private void SetShipDetails()
+        {
+            ShipData shipData = ships[currentShipIndex];
+            bool isUnlocked = ShopManager.instance.IsShipUnlocked(shipData);
+
+            shipName.text = shipData.GetShipName();
+            description.text = shipData.GetDescription();
+            speed.text = "Speed: " + shipData.GetSpeed().ToString();
+            acceleration.text = "Acceleration: " + shipData.GetSpeedIncreaseRate().ToString();
+            turnSpeed.text = "Turn Speed: " + shipData.GetTurningSpeed().ToString();
+            cost.text = "COST: " + shipData.GetCost().ToString() + "P";
+            specialAbility.text = shipData.GetSpecialAbility().ToString();
+            specialAbilityDescription.text = shipData.GetSpecialAbilityDescription();
+            shipIcon.sprite = shipData.GetSprite();
+
+            if (!isUnlocked)
+            {
+                shipIcon.color = Color.black;
+            }
+        }
+    }
 }
