9bb8dba629753528915d65189b9b270acbe77d32
Tom Hewitt <tommhewitt@gmail.com>
2020-02-29 20:50:21 +0000

    💻 Add a hyperdrive powerup

---
+++ Assets/Scripts/Spawnable Object/Powerups/Hyperdrive.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace SpawnableObject.Powerups
+{
+	/// <summary>
+	/// Makes the player jump really high to avoid obstacles.
+	/// <summary>
+	public class Hyperdrive : Powerup
+	{
+		[SerializeField] private float distance;
+
+		public override void ApplyPowerupEffect()
+		{
+		
+		}
+	}
+}
