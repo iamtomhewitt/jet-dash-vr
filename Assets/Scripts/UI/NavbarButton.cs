928429ee85d1a4d99eac8b386061e5579dbb66e3
Tom Hewitt <tommhewitt@gmail.com>
2020-02-29 20:32:11 +0000

    💻 Update the nav bar so the active sprite is shown dependent on the scene name

--- Assets/Scripts/UI/NavbarButton.cs
+++ Assets/Scripts/UI/NavbarButton.cs
@@ -1,10 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using LevelManagers;
+using UnityEngine.SceneManagement;
 
 namespace UI
 {
 	public class NavbarButton : MonoBehaviour
 	{
+		[SerializeField] private string sceneName;
+		[SerializeField] private Image activeSprite;
+
+		private void Start()
+		{
+			if (SceneManager.GetActiveScene().name.Equals(sceneName))
+			{
+				activeSprite.enabled = true;
+			}
+			else
+			{
+				activeSprite.enabled = false;
+			}
+		}
+
 		/// <summary>
 		/// Called from a Unity button. Used for the navbar at the bottom of a screen, saves
 		/// us having to hook up a level manager on each scene. This way the prefab will always be
