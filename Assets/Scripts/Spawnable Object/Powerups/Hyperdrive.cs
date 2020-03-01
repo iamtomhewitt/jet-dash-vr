91e88d8ac0f7c7f390c3cebbb762ca98acfc8afc
Tom Hewitt <tommhewitt@gmail.com>
2020-03-01 21:15:19 +0000

    💻 Increase the field of view of the camera to make it look like we are going really fast

--- Assets/Scripts/Spawnable Object/Powerups/Hyperdrive.cs
+++ Assets/Scripts/Spawnable Object/Powerups/Hyperdrive.cs
@@ -1,18 +1,59 @@
 using UnityEngine;
+using System.Collections;
+using Player;
 
 namespace SpawnableObject.Powerups
 {
-	/// <summary>
-	/// Makes the player jump really high to avoid obstacles.
-	/// <summary>
-	public class Hyperdrive : Powerup
-	{
-		[SerializeField] private float distance;
+    /// <summary>
+    /// Makes the player jump really high to avoid obstacles.
+    /// <summary>
+    public class Hyperdrive : Powerup
+    {
+        [SerializeField] private float speedFieldOfView = 110f;
+        [SerializeField] private float brakeFieldOfView = 45f;
+        [SerializeField] private float zoomSpeed = 100f;
 
-		public override void ApplyPowerupEffect()
+        public override void ApplyPowerupEffect()
+        {
+            PlayerHud.instance.ShowNotification(GetColour(), "Hyperdrive!");
+            StartCoroutine(Routine());
+        }
+
+        private IEnumerator Routine()
+        {           
+            Camera camera = Camera.main;
+			float originalFov = 60f;
+
+            yield return StartCoroutine(IncreaseFov(camera, speedFieldOfView, zoomSpeed));
+            yield return new WaitForSeconds(1f);
+            yield return StartCoroutine(DecreaseFov(camera, brakeFieldOfView, zoomSpeed));
+            yield return StartCoroutine(IncreaseFov(camera, originalFov, zoomSpeed));
+
+            camera.fieldOfView = originalFov;
+        }
+
+		private IEnumerator IncreaseFov(Camera camera, float target, float speed)
 		{
-			// TODO - alter the field of view of the camera to make it look like the hyperdrive is distorting
-			// Do it with animation or with coding?
+			do
+            {
+                camera.fieldOfView += Time.deltaTime * speed;
+                yield return null;
+            }
+            while (camera.fieldOfView < target);
+
+            camera.fieldOfView = target;
 		}
-	}
+
+		private IEnumerator DecreaseFov(Camera camera, float target, float speed)
+		{
+			do
+            {
+                camera.fieldOfView -= Time.deltaTime * speed;
+                yield return null;
+            }
+            while (camera.fieldOfView > target);
+
+            camera.fieldOfView = target;
+		}
+    }
 }
