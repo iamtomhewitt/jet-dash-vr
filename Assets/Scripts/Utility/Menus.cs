d59bba6cc85da5c8e9d195903ae0996d04b05ac6
Tom Hewitt <tommhewitt@gmail.com>
2020-03-01 20:37:07 +0000

    🎨 Make nav bar buttons wider

--- Assets/Scripts/Utility/Menus.cs
+++ Assets/Scripts/Utility/Menus.cs
@@ -1,14 +1,16 @@
+using SimpleJSON;
+
 namespace Utility
 {
-	public class Menus
-	{
-		public const string ACHIEVEMENTS = "Achievements";
-		public const string GAME = "Game";
-		public const string HELP = "Help";
-		public const string MAIN_MENU = "Main Menu";
-		public const string ONLINE_HIGHSCORES = "Online Highscores";
-		public const string PRELOAD = "Preload";
-		public const string SETTINGS = "Settings";
-		public const string SHOP = "Shop";
-	}
+    public class Menus
+    {
+        public const string ACHIEVEMENTS = "Achievements";
+        public const string GAME = "Game";
+        public const string HELP = "Help";
+        public const string MAIN_MENU = "Main Menu";
+        public const string ONLINE_HIGHSCORES = "Online Highscores";
+        public const string PRELOAD = "Preload";
+        public const string SETTINGS = "Settings";
+        public const string SHOP = "Shop";
+    }
 }
