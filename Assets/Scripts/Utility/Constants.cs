f195d40c7fb41ab20e7c96d6dbffeaf7911d5514
Tom Hewitt <tommhewitt@gmail.com>
2020-03-03 15:21:50 +0000

    💎 Performance updates on PlayerHud

--- Assets/Scripts/Utility/Constants.cs
+++ Assets/Scripts/Utility/Constants.cs
@@ -9,6 +9,8 @@
 		public const string CASH_KEY				= "cash";
 		public const string DREAMLO_URL				= "http://dreamlo.com/lb/";
 		public const string STARTING_SHIP			= "Rescate";
+		public const string POWERUP_NOTIFY_SHOW		= "Powerup Notification Show";
+		public const string POWERUP_NOTIFY_HIDE		= "Powerup Notification Hide";
 
 		public const int YES = 1;
 		public const int NO = 0;
