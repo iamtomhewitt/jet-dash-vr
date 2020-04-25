08560e384cf78ef7ffaa23c69c39fccb1196c32d
Tom Hewitt <tommhewitt@gmail.com>
2020-04-25 18:05:05 +0100

    🎨 Add tabs for showing different leaderboards

---
+++ Assets/Scripts/UI/LeaderboardTab.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Utility;
+
+namespace UI
+{
+    public class LeaderboardTab : MonoBehaviour
+    {
+        [SerializeField] private Color activeButtonColour;
+        [SerializeField] private Color activeTextColour;
+
+        private GameObject[] leaderboards;
+        private GameObject[] tabButtons;
+
+        private void Start()
+        {
+            leaderboards = GameObject.FindGameObjectsWithTag(Tags.LEADERBOARD);
+            tabButtons = GameObject.FindGameObjectsWithTag(Tags.LEADERBOARD_TAB);
+        }
+
+        public void ShowLeaderboard(GameObject leaderboard)
+        {
+            HideAllLeaderboards();
+            DeactivateAllButtons();
+            ActivateButton(this.GetComponent<Button>());
+            leaderboard.SetActive(true);
+        }
+
+        private void HideAllLeaderboards()
+        {
+            foreach (GameObject leaderboard in leaderboards)
+            {
+                leaderboard.SetActive(false);
+            }
+        }    
+
+        private void ActivateButton(Button button)
+        {
+            button.GetComponent<Image>().color = activeButtonColour;
+            button.GetComponentInChildren<Text>().color = activeTextColour;
+        }
+
+        private void DeactivateAllButtons()
+        {
+            foreach (GameObject button in tabButtons)
+            {
+                DeactivateButton(button.GetComponent<Button>());
+            }
+        }
+
+        private void DeactivateButton(Button button)
+        {
+            // All we do is invert the colours
+            button.GetComponent<Image>().color = activeTextColour;
+            button.GetComponentInChildren<Text>().color = activeButtonColour;
+        }
+    }
+}
--- Assets/Scripts/Utility/Tags.cs
+++ Assets/Scripts/Utility/Tags.cs
@@ -5,6 +5,8 @@
 		public const string OBSTACLE = "Obstacle";
 		public const string POWERUP = "Powerup";
 		public const string PLAYER = "Player";
+		public const string LEADERBOARD_TAB = "Leaderboard Tab";
+		public const string LEADERBOARD = "Leaderboard";
 		public const string CELLEX = "Cellex";
 		public const string SOLSTICE = "Solstice";
 		public const string DEVILDOG = "DevilDog";
