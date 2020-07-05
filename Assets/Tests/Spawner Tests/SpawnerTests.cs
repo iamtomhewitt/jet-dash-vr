6608c39ef6db9f6590081c2ac2aefc414e3297ad
Tom Hewitt <tommhewitt@gmail.com>
2020-07-05 16:26:30 +0100

    test(#67): add spawner tests

---
+++ Assets/Tests/Spawner Tests/SpawnerTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using UnityEngine.TestTools;
+using UnityEngine;
+using Utility;
+
+namespace Tests
+{
+    public class SpawnerTests
+    {
+        private GameObject obstacle;
+        private GameObject obstacleSpawner;
+        private GameObject obstacleParent;
+        private GameObject player;
+
+        [SetUp]
+        public void Setup()
+        {
+            obstacle = MonoBehaviour.Instantiate(TestConstants.GetResource("Obstacle"));
+            obstacleSpawner = MonoBehaviour.Instantiate(TestConstants.GetResource("Spawners/Obstacle Spawner"));
+            obstacleParent = MonoBehaviour.Instantiate(new GameObject("Obstacles"));
+            player = MonoBehaviour.Instantiate(new GameObject("Player"));
+
+            obstacleSpawner.GetComponent<MatchTransformPosition>().SetTarget(player.transform);
+            player.tag = "Player";
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Object.Destroy(obstacle);
+            Object.Destroy(obstacleSpawner.gameObject);
+            Object.Destroy(obstacleParent);
+            Object.Destroy(player);
+        }
+
+        [UnityTest]
+        public IEnumerator ShouldRelocateObstacle()
+        {
+            Vector3 pos = obstacle.transform.position;
+            obstacle.transform.position = obstacleSpawner.transform.position;
+            yield return new WaitForSeconds(TestConstants.WAIT_TIME);
+            Assert.AreNotEqual(pos, obstacle.transform.position);
+        }
+    }
+}
