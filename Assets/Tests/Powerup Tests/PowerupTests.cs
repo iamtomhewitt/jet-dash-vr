63bf63ec9e7f4166ef4d9d8fa0a4a4c2c5b8ad54
Tom Hewitt <tommhewitt@gmail.com>
2020-07-06 14:58:21 +0100

    test(#82): add speed up powerup tests

--- Assets/Tests/Powerup Tests/PowerupTests.cs
+++ Assets/Tests/Powerup Tests/PowerupTests.cs
@@ -1,139 +1,151 @@
-﻿using Data;
-using Manager;
+﻿using Manager;
 using NUnit.Framework;
 using Player;
 using System.Collections;
 using UnityEngine.TestTools;
 using UnityEngine;
-using Utility;
 using SpawnableObject.Powerups;
 
 namespace Tests
 {
-    public class PowerupTests
-    {
-        private AudioManager am;
-        private AchievementManager achm;
-        private GameObject player;
-        private GameObject bp;
-        private GameObject dpp;
-        private GameObject hp;
-        private GameObject ip;
-        private GameObject jp;
-        private GameSettingsManager gs;
-        private PlayerCollision pc;
-        private PlayerScore ps;
-        private ShopManager sm;
+	public class PowerupTests
+	{
+		private AchievementManager achm;
+		private AudioManager am;
+		private GameObject bp;
+		private GameObject dpp;
+		private GameObject hp;
+		private GameObject ip;
+		private GameObject jp;
+		private GameObject player;
+		private GameObject sup;
+		private GameSettingsManager gs;
+		private PlayerControl pcn;
+		private PlayerCollision pc;
+		private PlayerScore ps;
+		private ShopManager sm;
 
-        [SetUp]
-        public void Setup()
-        {
-            am = MonoBehaviour.Instantiate(TestConstants.GetResource("Managers/Audio Manager")).GetComponent<AudioManager>();
-            achm = MonoBehaviour.Instantiate(TestConstants.GetResource("Managers/Achievement Manager")).GetComponent<AchievementManager>();
-            gs = MonoBehaviour.Instantiate(TestConstants.GetResource("Managers/Game Settings")).GetComponent<GameSettingsManager>();
-            player = MonoBehaviour.Instantiate(TestConstants.GetResource("Player"));
-            player.transform.position = new Vector3(0f, 0f - 1000f);
+		[SetUp]
+		public void Setup()
+		{
+			am = MonoBehaviour.Instantiate(TestConstants.GetResource("Managers/Audio Manager")).GetComponent<AudioManager>();
+			achm = MonoBehaviour.Instantiate(TestConstants.GetResource("Managers/Achievement Manager")).GetComponent<AchievementManager>();
+			gs = MonoBehaviour.Instantiate(TestConstants.GetResource("Managers/Game Settings")).GetComponent<GameSettingsManager>();
+			player = MonoBehaviour.Instantiate(TestConstants.GetResource("Player"));
+			player.transform.position = new Vector3(0f, 0f - 1000f);
 
-            bp = MonoBehaviour.Instantiate(TestConstants.GetResource("Powerups/Bonus Points"));
-            dpp = MonoBehaviour.Instantiate(TestConstants.GetResource("Powerups/Double Points"));
-            hp = MonoBehaviour.Instantiate(TestConstants.GetResource("Powerups/Hyperdrive"));
-            ip = MonoBehaviour.Instantiate(TestConstants.GetResource("Powerups/Invincibility"));
-            jp = MonoBehaviour.Instantiate(TestConstants.GetResource("Powerups/Jump"));
-            sm = MonoBehaviour.Instantiate(TestConstants.GetResource("Managers/Shop Manager")).GetComponent<ShopManager>();
+			bp = MonoBehaviour.Instantiate(TestConstants.GetResource("Powerups/Bonus Points"));
+			dpp = MonoBehaviour.Instantiate(TestConstants.GetResource("Powerups/Double Points"));
+			hp = MonoBehaviour.Instantiate(TestConstants.GetResource("Powerups/Hyperdrive"));
+			ip = MonoBehaviour.Instantiate(TestConstants.GetResource("Powerups/Invincibility"));
+			jp = MonoBehaviour.Instantiate(TestConstants.GetResource("Powerups/Jump"));
+			sm = MonoBehaviour.Instantiate(TestConstants.GetResource("Managers/Shop Manager")).GetComponent<ShopManager>();
+			sup = MonoBehaviour.Instantiate(TestConstants.GetResource("Powerups/Speed Up"));
 
-            pc = player.GetComponent<PlayerCollision>();
-            ps = player.GetComponent<PlayerScore>();
-        }
+			pc = player.GetComponent<PlayerCollision>();
+			pcn = player.GetComponent<PlayerControl>();
+			ps = player.GetComponent<PlayerScore>();
+		}
 
-        [TearDown]
-        public void Teardown()
-        {
-            Object.Destroy(am);
-            Object.Destroy(achm);
-            Object.Destroy(gs);
-            Object.Destroy(player);
-            Object.Destroy(bp);
-            Object.Destroy(dpp);
-            Object.Destroy(hp);
-            Object.Destroy(ip);
-            Object.Destroy(jp);
-            Object.Destroy(sm);
-        }
+		[TearDown]
+		public void Teardown()
+		{
+			Object.Destroy(achm);
+			Object.Destroy(am);
+			Object.Destroy(bp);
+			Object.Destroy(dpp);
+			Object.Destroy(gs);
+			Object.Destroy(hp);
+			Object.Destroy(ip);
+			Object.Destroy(jp);
+			Object.Destroy(player);
+			Object.Destroy(sm);
+			Object.Destroy(sup);
+		}
 
-        [UnityTest]
-        public IEnumerator ShouldAddBonusPoints()
-        {
-            MakePlayerCollideWith(bp);
-            yield return new WaitForSeconds(TestConstants.WAIT_TIME);
-            Assert.AreEqual(500, ps.GetBonusScore());
-        }
+		[UnityTest]
+		public IEnumerator ShouldAddBonusPoints()
+		{
+			MakePlayerCollideWith(bp);
+			yield return new WaitForSeconds(TestConstants.WAIT_TIME);
+			Assert.AreEqual(500, ps.GetBonusScore());
+		}
 
-        [UnityTest]
-        public IEnumerator ShouldGivePointsWhenCollidingWithDoublePointsAndZeroScore()
-        {
-            MakePlayerCollideWith(dpp);
-            yield return new WaitForSeconds(TestConstants.WAIT_TIME);
-            Assert.AreEqual(500, ps.GetBonusScore());
-        }
+		[UnityTest]
+		public IEnumerator ShouldGivePointsWhenCollidingWithDoublePointsAndZeroScore()
+		{
+			MakePlayerCollideWith(dpp);
+			yield return new WaitForSeconds(TestConstants.WAIT_TIME);
+			Assert.AreEqual(500, ps.GetBonusScore());
+		}
 
-        [UnityTest]
-        public IEnumerator ShouldDoublePoints()
-        {
-            ps.AddBonusPoints(1000);
-            MakePlayerCollideWith(dpp);
-            yield return new WaitForSeconds(TestConstants.WAIT_TIME);
-            Assert.AreEqual(2000, ps.GetBonusScore());
-        }
+		[UnityTest]
+		public IEnumerator ShouldDoublePoints()
+		{
+			ps.AddBonusPoints(1000);
+			MakePlayerCollideWith(dpp);
+			yield return new WaitForSeconds(TestConstants.WAIT_TIME);
+			Assert.AreEqual(2000, ps.GetBonusScore());
+		}
 
-        [UnityTest]
-        public IEnumerator ShouldMakePlayerInvincible()
-        {
-            MakePlayerCollideWith(ip);
-            yield return new WaitForSeconds(TestConstants.WAIT_TIME);
-            Assert.True(pc.IsGodMode());
-            Assert.True(pc.GetShield().activeSelf);
-        }
+		[UnityTest]
+		public IEnumerator ShouldMakePlayerInvincible()
+		{
+			MakePlayerCollideWith(ip);
+			yield return new WaitForSeconds(TestConstants.WAIT_TIME);
+			Assert.True(pc.IsGodMode());
+			Assert.True(pc.GetShield().activeSelf);
+		}
 
-        [UnityTest]
-        public IEnumerator ShouldMakePlayerGodModeWhenHyperdriving()
-        {
-            MakePlayerCollideWith(hp);
-            yield return new WaitForSeconds(TestConstants.WAIT_TIME);
-            Assert.True(pc.IsGodMode());
-        }
+		[UnityTest]
+		public IEnumerator ShouldIncreaseSpeed()
+		{
+			float s = pcn.GetSpeed();
+			MakePlayerCollideWith(sup);
+			yield return new WaitForSeconds(TestConstants.WAIT_TIME);
+			Assert.AreEqual(s + 20f, pcn.GetSpeed());
+		}
 
-        [UnityTest]
-        public IEnumerator ShouldMakePlayerJump()
-        {
-            float y = player.transform.position.y;
-            MakePlayerCollideWith(jp);
-            yield return new WaitForSeconds(TestConstants.WAIT_TIME);
-            Assert.GreaterOrEqual(player.transform.position.y, y);
-        }
+		[UnityTest]
+		public IEnumerator ShouldMakePlayerGodModeWhenHyperdriving()
+		{
+			MakePlayerCollideWith(hp);
+			yield return new WaitForSeconds(TestConstants.WAIT_TIME);
+			Assert.True(pc.IsGodMode());
+		}
 
-        [UnityTest]
-        public IEnumerator ShouldRelocateAfterCollision()
-        {
-            Vector3 pos = bp.transform.position;
-            MakePlayerCollideWith(bp);
-            yield return new WaitForSeconds(TestConstants.WAIT_TIME);
-            Assert.AreNotEqual(pos, bp.transform.position);
-        }
+		[UnityTest]
+		public IEnumerator ShouldMakePlayerJump()
+		{
+			float y = player.transform.position.y;
+			MakePlayerCollideWith(jp);
+			yield return new WaitForSeconds(TestConstants.WAIT_TIME);
+			Assert.GreaterOrEqual(player.transform.position.y, y);
+		}
 
-        [UnityTest]
-        public IEnumerator ShouldRelocateIfBehindPlayer()
-        {
-            bp.transform.position = new Vector3(0f, 0f, -2000f);
-            Vector3 pos = bp.transform.position;
-            bp.GetComponent<BonusPoints>().RelocateIfBehindPlayer();
-            yield return new WaitForSeconds(TestConstants.WAIT_TIME);
-            Assert.AreNotEqual(pos, bp.transform.position);
-            Assert.GreaterOrEqual(bp.transform.position.z, player.transform.position.z);
-        }
+		[UnityTest]
+		public IEnumerator ShouldRelocateAfterCollision()
+		{
+			Vector3 pos = bp.transform.position;
+			MakePlayerCollideWith(bp);
+			yield return new WaitForSeconds(TestConstants.WAIT_TIME);
+			Assert.AreNotEqual(pos, bp.transform.position);
+		}
 
-        private void MakePlayerCollideWith(GameObject gameObject)
-        {
-            player.transform.position = gameObject.transform.position;
-        }
-    }
+		[UnityTest]
+		public IEnumerator ShouldRelocateIfBehindPlayer()
+		{
+			bp.transform.position = new Vector3(0f, 0f, -2000f);
+			Vector3 pos = bp.transform.position;
+			bp.GetComponent<BonusPoints>().RelocateIfBehindPlayer();
+			yield return new WaitForSeconds(TestConstants.WAIT_TIME);
+			Assert.AreNotEqual(pos, bp.transform.position);
+			Assert.GreaterOrEqual(bp.transform.position.z, player.transform.position.z);
+		}
+
+		private void MakePlayerCollideWith(GameObject gameObject)
+		{
+			player.transform.position = gameObject.transform.position;
+		}
+	}
 }
