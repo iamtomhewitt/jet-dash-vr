3731e1f8bfb9f853b70729e79612a9763812f2b5
Tom Hewitt <tommhewitt@gmail.com>
2020-07-08 15:41:48 +0100

    feat(#86): add rate me popup manager

---
+++ Assets/Scripts/Managers/RateMePopupManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace Manager
+{
+	public class RateMePopupManager : MonoBehaviour
+	{
+		[SerializeField] private GameObject popupVertical;
+		[SerializeField] private GameObject popupHorizontal;
+
+		private int showCount = 0;
+		private const int NUMBER_OF_TIMES_BEFORE_POPUP_SHOW = 25;
+
+		public static RateMePopupManager instance;
+
+		private void Awake()
+		{
+			if (instance)
+			{
+				DestroyImmediate(gameObject);
+			}
+			else
+			{
+				DontDestroyOnLoad(gameObject);
+				instance = this;
+			}
+		}
+
+		private void Start()
+		{
+			popupHorizontal.SetActive(false);
+			popupVertical.SetActive(false);
+		}
+
+		public void IncreaseShowCount()
+		{
+			showCount++;
+		}
+
+		public void ShowRateMePopupIfAllowed()
+		{
+			if (showCount > NUMBER_OF_TIMES_BEFORE_POPUP_SHOW)
+			{
+				bool isHorizontal = (Screen.orientation == ScreenOrientation.Landscape) ? true : false;
+				popupHorizontal.SetActive(isHorizontal);
+				popupVertical.SetActive(!isHorizontal);
+				showCount = 0;
+			}
+		}
+	}
+}
