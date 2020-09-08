9335e8c8a30ae20f9e5663f856635b3290e2bb08
Tom Hewitt <tommhewitt@gmail.com>
2020-09-08 21:22:33 +0100

    feat(#81): make animation play when player passes nearby relative to speed

--- Assets/Scripts/Spawnable Object/AnimatedObstacle.cs
+++ Assets/Scripts/Spawnable Object/AnimatedObstacle.cs
@@ -1,36 +1,51 @@
+using Player;
 using UnityEngine;
 using Utility;
 
 namespace SpawnableObject
 {
-    /// <summary>
-    /// An obstacle that the player has to dodge.
-    /// </summary>
-    public class AnimatedObstacle : SpawnableObject
-    {
-        [SerializeField] private Animator animator;
+	public class AnimatedObstacle : SpawnableObject
+	{
+		[SerializeField] private Animator animator;
 
-        private const string ANIMATION_EMTPY = "Empty";
-        private const string ANIMATION_NAME = "Animating";
+		private PlayerControl playerControl;
+		private SphereCollider activationCollider;
+		private float activationRadius = 20f;
 
-        public override void AfterRelocation()
-        {
-            animator.Play(ANIMATION_EMTPY);
-        }
+		private const string ANIMATION_EMTPY = "Empty";
+		private const string ANIMATION_NAME = "Animating";
 
-        private void OnTriggerEnter(Collider other)
-        {
-            switch (other.gameObject.tag)
-            {
-                case Tags.PLAYER:
-                    print("should animate");
-                    animator.Play(ANIMATION_NAME);
-                    break;
+		public override void Start()
+		{
+			base.Start();
+			activationCollider = GetComponent<SphereCollider>();
+			playerControl = FindObjectOfType<PlayerControl>();
+			InvokeRepeating("IncreaseRadius", 0f, 1f);
+		}
 
-                default:
-                    // Nothing to do!
-                    break;
-            }
-        }
-    }
+		private void IncreaseRadius()
+		{
+			float newRadius = (playerControl.GetSpeed() * 1.5f) + activationRadius;
+			activationCollider.radius = newRadius;
+		}
+
+		public override void AfterRelocation()
+		{
+			animator.Play(ANIMATION_EMTPY);
+		}
+
+		private void OnTriggerEnter(Collider other)
+		{
+			switch (other.gameObject.tag)
+			{
+				case Tags.PLAYER:
+					animator.Play(ANIMATION_NAME);
+					break;
+
+				default:
+					// Nothing to do!
+					break;
+			}
+		}
+	}
 }
--- Assets/Scripts/Spawners/ObstacleSpawner.cs
+++ Assets/Scripts/Spawners/ObstacleSpawner.cs
@@ -4,96 +4,105 @@ using Utility;
 
 namespace Spawner
 {
-    /// <summary>
-    /// This GameObject has a large wall like collider attached to it. Think of it like a giant broomstick.
-    /// When an obstacle hits the collider, it is repositioned. This means we are not continuously Destroying and Instantiating objects,
-    /// we are reusing ones we have, saving on computing power on a mobile.
-    /// </summary>
-    public class ObstacleSpawner : MonoBehaviour
-    {
-        [SerializeField] private GameObject[] obstacles;
-        [SerializeField] private ObstacleSize obstacleSize;
-        [SerializeField] private Material[] colours;
-        [SerializeField] private int totalObstacles;
-        [Space()]
-        [SerializeField] private SpawnBoundary boundary;
-        [SerializeField] private SpawnBoundary initialBoundary;
+	/// <summary>
+	/// This GameObject has a large wall like collider attached to it. Think of it like a giant broomstick.
+	/// When an obstacle hits the collider, it is repositioned. This means we are not continuously Destroying and Instantiating objects,
+	/// we are reusing ones we have, saving on computing power on a mobile.
+	/// </summary>
+	public class ObstacleSpawner : MonoBehaviour
+	{
+		[SerializeField] private GameObject[] obstacles;
+		[SerializeField] private ObstacleSize obstacleSize;
+		[SerializeField] private Material[] colours;
+		[SerializeField] private int totalObstacles;
+		[Space()]
+		[SerializeField] private SpawnBoundary boundary;
+		[SerializeField] private SpawnBoundary initialBoundary;
 
-        private const string OBSTACLE_PARENT = "Obstacles";
-        private const float SPAWN_REDUCTION_TIME = 30f;
+		private const string OBSTACLE_PARENT = "Obstacles";
+		private const float SPAWN_REDUCTION_TIME = 30f;
 
-        private void Start()
-        {
-            InitialiseObstacles();
-            InvokeRepeating("ReduceSpawnBoundary", SPAWN_REDUCTION_TIME, SPAWN_REDUCTION_TIME);
-        }
+		private void Start()
+		{
+			InitialiseObstacles();
+			InvokeRepeating("ReduceSpawnBoundary", SPAWN_REDUCTION_TIME, SPAWN_REDUCTION_TIME);
+		}
 
-        /// <summary>
-        /// Instantiates all the obstacles that will be used in the game.
-        /// </summary>
-        private void InitialiseObstacles()
-        {
-            for (int i = 0; i < totalObstacles; i++)
-            {
-                // Work out a scale and how high the cube has to be so it sits nicely on top of the floor
-                int scale = Random.Range(obstacleSize.minSize, obstacleSize.maxSize);
-                float y = (scale / 2) + 0.5f;
+		/// <summary>
+		/// Instantiates all the obstacles that will be used in the game.
+		/// </summary>
+		private void InitialiseObstacles()
+		{
+			for (int i = 0; i < totalObstacles; i++)
+			{
+				GameObject o = Instantiate(obstacles[Random.Range(0, obstacles.Length)], GameObject.Find(OBSTACLE_PARENT).transform) as GameObject;
+				Renderer r = (Renderer)o.GetInChildIfNotOnThisComponent("UnityEngine.Renderer, UnityEngine");
+				r.material = colours[Random.Range(0, colours.Length)];
 
-                GameObject o = Instantiate(obstacles[Random.Range(0, obstacles.Length)], GenerateObstaclePosition(initialBoundary, y), Quaternion.identity) as GameObject;
-                o.GetComponent<StationaryObstacle>().Grow(scale);
-                o.GetComponent<Renderer>().material = colours[Random.Range(0, colours.Length)];
-                o.transform.localScale = new Vector3(scale, scale, scale);
-                o.transform.parent = GameObject.Find(OBSTACLE_PARENT).transform;
-            }
-        }
+				if (o.GetComponent<StationaryObstacle>())
+				{
+					// Work out a scale and how high the cube has to be so it sits nicely on top of the floor
+					int scale = Random.Range(obstacleSize.minSize, obstacleSize.maxSize);
+					float y = (scale / 2) + 0.5f;
 
-        /// <summary>
-        /// Generates a position for the obstacle. The y value is calculated beforehand to get the height right.
-        /// </summary>
-        private Vector3 GenerateObstaclePosition(SpawnBoundary b, float y)
-        {
-            float x = Random.Range(b.xMin, b.xMax);
-            float z = Random.Range(b.zMin, b.zMax);
+					o.transform.SetPosition(GenerateObstaclePosition(initialBoundary, y));
+					o.GetComponent<StationaryObstacle>().Grow(scale);
+					o.transform.localScale = new Vector3(scale, scale, scale);
+				}
+				if (o.GetComponent<AnimatedObstacle>())
+				{
+					o.transform.SetPosition(GenerateObstaclePosition(initialBoundary, o.transform.position.y));
+				}
+			}
+		}
 
-            return new Vector3(transform.position.x + x, y, transform.position.z + z);
-        }
+		/// <summary>
+		/// Generates a position for the obstacle. The y value is calculated beforehand to get the height right.
+		/// </summary>
+		private Vector3 GenerateObstaclePosition(SpawnBoundary b, float y)
+		{
+			float x = Random.Range(b.xMin, b.xMax);
+			float z = Random.Range(b.zMin, b.zMax);
 
-        /// <summary>
-        /// Reduces the spawn boundary to make the cubes appear tighter, making the game harder.
-        /// </summary>
-        private void ReduceSpawnBoundary()
-        {
-            if (boundary.CanShrinkZMin())
-            {
-                boundary.ReduceZMin();
-            }
+			return new Vector3(transform.position.x + x, y, transform.position.z + z);
+		}
 
-            if (boundary.CanShrinkZMax())
-            {
-                boundary.ReduceZMax();
-            }
-        }
+		/// <summary>
+		/// Reduces the spawn boundary to make the cubes appear tighter, making the game harder.
+		/// </summary>
+		private void ReduceSpawnBoundary()
+		{
+			if (boundary.CanShrinkZMin())
+			{
+				boundary.ReduceZMin();
+			}
 
-        private void OnTriggerEnter(Collider other)
-        {
-            switch (other.gameObject.tag)
-            {
-                case Tags.OBSTACLE:
-                    SpawnableObject.SpawnableObject o = (SpawnableObject.SpawnableObject)other.GetInParentIfNotOnThisComponent("SpawnableObject.SpawnableObject");
-                    o.Relocate();
-                    o.AfterRelocation();
-                    break;
+			if (boundary.CanShrinkZMax())
+			{
+				boundary.ReduceZMax();
+			}
+		}
 
-                default:
-                    // Nothing to do!
-                    break;
-            }
-        }
-    }
+		private void OnTriggerEnter(Collider other)
+		{
+			switch (other.gameObject.tag)
+			{
+				case Tags.OBSTACLE:
+					SpawnableObject.SpawnableObject o = (SpawnableObject.SpawnableObject)other.GetInParentIfNotOnThisComponent("SpawnableObject.SpawnableObject");
+					o.Relocate();
+					o.AfterRelocation();
+					break;
 
-    [System.Serializable]
-    public class ObstacleSize
-    {
-        public int minSize, maxSize;
-    }
+				default:
+					// Nothing to do!
+					break;
+			}
+		}
+	}
+
+	[System.Serializable]
+	public class ObstacleSize
+	{
+		public int minSize, maxSize;
+	}
 }
